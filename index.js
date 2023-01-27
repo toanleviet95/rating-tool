9b2c778cdc672e6562137dc330dbd05b29d48586
tony-le <>
2023-01-28 04:52:32 +0700

    Update code

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import { signOut } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+import { initSignOut, initUserInfo } from './common.js';
 import { app, auth } from './firebase-init.js';
+import config from './config.js';
 
 const stars = document.querySelectorAll(".star");
 const emoji = document.querySelector(".emoji");
 const rating = document.querySelector(".rating");
 const defaultRatingIndex = 0;
 let currentRatingIndex = 0;
+let currentUser = '';
+let currentData = [];
+let currentSourceId = '';
 
 const ratings = [
   { emoji: "😐", status: "Rating" },
@@ -37,6 +42,7 @@ function checkRating(star) {
 }
 
 function resetRating() {
+  $(".save-btn").prop( "disabled", true);
   currentRatingIndex = defaultRatingIndex;
   setRating(defaultRatingIndex);
 }
@@ -50,6 +56,9 @@ function renderStars() {
       }
       const index = parseInt(star.getAttribute("data-rate"));
       currentRatingIndex = index;
+      const currentSlideIndex = $('.slider').slick('slickCurrentSlide');
+      currentSourceId = currentData && currentData[currentSlideIndex] ? currentData[currentSlideIndex].source_id : '';
+      $(".save-btn").prop( "disabled", false);
       setRating(index);
     });
   
@@ -64,10 +73,40 @@ function renderStars() {
   });
 }
 
+function initData() {
+  $('.lds-ring').show();
+  $('.info').hide();
+  axios.get(`${config.API_ENDPOINT}/rating_ui/${currentUser}`)
+  .then(function (response) {
+    $('.info').show();
+    const { data = [] } = response?.data;
+    currentData = data;
+    let htmlContent = '';
+    data.forEach(item => {
+      htmlContent += `
+      <div data-source="${item.source_id}">
+        <p class="prompt">${item.promt ? '"' + item.promt + '"' : ''}</p>
+        <a href="${item.url}" target="_blank">
+          <img src="${item.url}" alt="${item.promt}">
+        </a>
+      </div>
+      `
+    });
+    $('#slider').html(htmlContent);
+    renderSlide();
+  })
+  .catch(function (error) {
+    $('#slider').html('<p class="error">Data load failed. Something was wrong!!!</p>');
+    console.log(error);
+  })
+  .finally(function () {
+    $('.lds-ring').hide();
+  });
+}
+
 function renderSlide() {
   $(".slider").slick({
     infinite: true,
-    autoplay: true,
     autoplaySpeed: 2000,
   });
 }
@@ -81,33 +120,37 @@ function initFeedBackEvent() {
 function initSaveEvent() {
   $(".save-btn").click(function(e) {
     e.preventDefault();
-    console.log({ currentRatingIndex, "input": $('.feedback-textarea').val() });
-  });
-}
-
-function initUserInfo() {
-  if (localStorage.getItem('email')) {
-    $('#email').html(localStorage.getItem('email'));
-  }
-}
-
-function initSignOut() {
-  $("#sign-out").click(function(e) {
-    localStorage.removeItem('token');
-    localStorage.removeItem('uid');
-    localStorage.removeItem('email');
-    signOut(auth);
-    window.location.href = '/sign-in.html';
+    $('.save').html('<p>Saving...</p>');
+    axios.post(`${config.API_ENDPOINT}/saving_ui/`, {
+      "source_id": currentSourceId,
+      "username": currentUser,
+      "rating": currentRatingIndex,
+      "comment": $('.feedback-textarea').val()
+    })
+    .then(function (response) {
+      $('.save').html('<p class="success">Your rating was saved successfully!</p>');
+      setTimeout(() => {
+        const currentSlideIndex = $('.slider').slick('slickCurrentSlide');
+        $('.slider').slick('slickRemove', currentSlideIndex);
+        // $(".slider").slick("slickNext");
+        $('.save').html('<button class="save-btn">SAVE</button>');
+        resetRating();
+        initSaveEvent();
+      }, 1000);
+    })
+    .catch(function (error) {
+      $('.save').html('<p class="error">Your rating was failed!!!</p>');
+    });
   });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderSlide();
+  currentUser = initUserInfo();
+  initSignOut();
+  initData();
   renderStars();
   setRating(defaultRatingIndex);
   initFeedBackEvent();
   initSaveEvent();
-  initUserInfo();
-  initSignOut();
 });
 
